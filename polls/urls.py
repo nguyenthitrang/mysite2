774aae4b6768f68b9aba113fa57cd3df6636505c
unknown <TrangNT41@Temp-TrangNT41.fsoft.fpt.vn>
2016-08-09 11:50:57 +0700

    write views that actualy do something

--- polls/urls.py
+++ polls/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
@@ -18,3 +23,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
