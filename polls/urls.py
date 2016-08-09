57e3913b91affdc573c3be82e7e06a9cb7c52ac5
unknown <TrangNT41@Temp-TrangNT41.fsoft.fpt.vn>
2016-08-09 16:18:29 +0700

    removing hardcoded URLs in templates

--- polls/urls.py
+++ polls/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # the 'name' value as called by the {% url %} template tag
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 ]
 
