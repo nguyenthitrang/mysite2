3e60418525293917eb53b18c98cf7b7cd167cec7
unknown <TrangNT41@Temp-TrangNT41.fsoft.fpt.vn>
2016-08-11 09:26:00 +0700

    write a simple form

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/ 
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
